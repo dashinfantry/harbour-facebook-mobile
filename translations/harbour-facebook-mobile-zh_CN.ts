c7bd178a8907598578fd6739448ff75510502780
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2020-10-01 00:02:01 +0800

    Create harbour-facebook-mobile-zh_CN.ts
    Chinese translation for Facebook Mobile app

---
+++ translations/harbour-facebook-mobile-zh_CN.ts
@@ -0,0 +1,193 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="19"/>
+        <source>Facebook Mobile translations</source>
+        <translation>Facebook Mobile 翻译</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="38"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="58"/>
+        <source>Copyright © 2019-2020 by Rudi Timmermans.</source>
+        <translation> Rudi Timmermans 版权所有 © 2019-2020 </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="62"/>
+        <source>Find me on Twitter</source>
+        <translation>在 Twitter 上找到我</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="73"/>
+        <source>Write a mail</source>
+        <translation>写电子邮件</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="86"/>
+        <source>Facebook Mobile is a Facebook WebView app for Sailfish OS.</source>
+        <translation>Facebook Mobile 是一个旗鱼系统 Facebook Webview 软件。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="89"/>
+        <source>Donations</source>
+        <translation>捐赠</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="94"/>
+        <source>If you like my work and want to buy me a beer, feel free to do it!</source>
+        <translation>如果你喜欢我的作品并且想给我买瓶啤酒，请勿拘束！</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="98"/>
+        <source>Donate with Paypal</source>
+        <translation>用 Paypal 捐赠</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="108"/>
+        <source>Licensing</source>
+        <translation许可证></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="112"/>
+        <source>Source code is available at GitHub. Translations, bug reports and other contributions are welcome!</source>
+        <translation>源代码位于 Github 。欢迎翻译、报告缺陷及其它贡献！</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="116"/>
+        <source>Sources on GitHub</source>
+        <translation>源代码位于 Github</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="127"/>
+        <source>Report an Issue</source>
+        <translation>报告问题</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="140"/>
+        <source>Fileman is free software released under the GNU General Public License (GPL), version 3 or later.</source>
+        <translation>Fileman 是一个采用 GNU 通用公共许可证（GPL）版本3及以上版本的自由软件。</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="8"/>
+        <source>My Cover</source>
+        <translation>我的封面</translation>
+    </message>
+</context>
+<context>
+    <name>FacebookPage</name>
+    <message>
+        <location filename="../qml/pages/FacebookPage.qml" line="89"/>
+        <source>About Facebook Mobile</source>
+        <translation>关于 Facebook Mobile</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FacebookPage.qml" line="95"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FacebookPage.qml" line="101"/>
+        <source>Hide navigation bar</source>
+        <translation隐藏导航栏></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FacebookPage.qml" line="102"/>
+        <source>Open navigation bar</source>
+        <translation>开启导航栏</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FacebookPage.qml" line="110"/>
+        <source>Facebook logout</source>
+        <translation>登出 Facebook</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FacebookPage.qml" line="117"/>
+        <source>Refresh</source>
+        <translation>刷新</translation>
+    </message>
+</context>
+<context>
+    <name>ImageSelectorPage</name>
+    <message>
+        <location filename="../qml/pages/ImageSelectorPage.qml" line="8"/>
+        <source>Upload image</source>
+        <translation>更新图片</translation>
+    </message>
+</context>
+<context>
+    <name>SelectionPanel</name>
+    <message>
+        <location filename="../qml/pages/components/SelectionPanel.qml" line="46"/>
+        <location filename="../qml/pages/components/SelectionPanel.qml" line="150"/>
+        <source>%1 selected</source>
+        <translation>已选择 %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/components/SelectionPanel.qml" line="72"/>
+        <location filename="../qml/pages/components/SelectionPanel.qml" line="176"/>
+        <source>%1 cut</source>
+        <translation>已剪切 %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/components/SelectionPanel.qml" line="84"/>
+        <location filename="../qml/pages/components/SelectionPanel.qml" line="189"/>
+        <source>%1 copied</source>
+        <translation>已复制 %1</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="23"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="25"/>
+        <source>Appearance</source>
+        <translation>外观</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="29"/>
+        <source>Theme</source>
+        <translation>主题</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="32"/>
+        <source>Ambience</source>
+        <translation>氛围</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="33"/>
+        <source>Facebook</source>
+        <translation>Facebook</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="34"/>
+        <source>Facebook nightmode</source>
+        <translation>Facebook 夜间模式</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="40"/>
+        <source>Show Facebook logout pulldown menu</source>
+        <translation>显示 Facebook 登出下拉菜单</translation>
+    </message>
+</context>
+<context>
+    <name>TranslationsPage</name>
+    <message>
+        <location filename="../qml/pages/TranslationsPage.qml" line="26"/>
+        <source>Facebook Mobile translators</source>
+        <translation>Facebook Mobile 翻译者</translation>
+    </message>
+</context>
+</TS>
